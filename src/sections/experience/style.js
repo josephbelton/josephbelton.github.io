1d44cc0a4524cfee3ae20a16a1fdd4d2dd7acc40
josephbelton <joseph.belton@sky.uk>
2021-06-06 21:24:24 +0100

    feat(home): update home to contain basic info

---
+++ src/sections/experience/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 0;
+    min-height: 100vh;
+    background-color: #cac4ce;
+`;
